f933ce6e6fc811d495579211f6a7ad7b2d897ec6
Jeremias <jeremias.souza@acreditarfidc.com.br>
2025-08-24 20:00:27 -0300

    feat: adicionar configuração do WhatsApp e implementar controle de testes para envio de mensagens

--- app/Services/WhatsAppService.php
+++ app/Services/WhatsAppService.php
@@ -8,11 +8,15 @@ class WhatsAppService
 {
     protected $token;
     protected $phoneId;
+    protected $apiPhoneNumber;
+    protected $apiVersion;
 
     public function __construct()
     {
-        $this->token = env('WHATSAPP_TOKEN');
-        $this->phoneId = env('WHATSAPP_PHONE_ID');
+        $this->token = config('whatsapp.token');
+        $this->phoneId = config('whatsapp.phone_id');
+        $this->apiPhoneNumber = config('whatsapp.api_phone_number'); 
+        $this->apiVersion = config('whatsapp.api_version');
     }
 
     public function sendMessageText($recipientNumber, $text, $previewUrl = true)
@@ -30,12 +34,17 @@ class WhatsAppService
 
         return $this->sendRequest($payload);
     }
-    
+
+
     protected function sendRequest($data)
     {
-        $url = "https://graph.facebook.com/v18.0/{$this->phoneId}/messages";
-        return Http::withToken($this->token)
-            ->post($url, $data)
-            ->json();
+        $url = "https://graph.facebook.com/{$this->apiVersion}/{$this->phoneId}/messages";
+
+        $response = Http::withHeaders([
+            "Authorization" => "Bearer {$this->token}",
+            "Content-Type" => "application/json",
+        ])->post($url, $data);
+
+        return $response->json();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\GeminiTestController;
+use App\Http\Controllers\WhatsAppTestController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -24,6 +26,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('/teste-whatsapp', [WhatsAppTestController::class, 'sendTest']);
+Route::get('/teste-gemini', [GeminiTestController::class, 'sendTest']);
+
+
 
 require __DIR__.'/auth.php';
 
