c9dbf15ff00a64c310f14f63cdc15b236924c3ca
Jeremias <jeremias.souza@acreditarfidc.com.br>
2025-08-24 18:50:42 -0300

    refactor: ajustar manipulação de dados do webhook e remover registro de requisições em arquivo JSON

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,12 +9,10 @@ class WebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        $webhookData = $request->getContent();
+        $webhookData = $request->all();
 
-        file_put_contents(storage_path('app/webhook_data.json'), json_encode($webhookData, JSON_PRETTY_PRINT));
-
-        // $processor = new WhatsAppWebhookProcessor();
-        // $result = $processor->process($webhookData);
+        $processor = new WhatsAppWebhookProcessor();
+        $result = $processor->process($webhookData);
 
         return response()->json(['status' => 'ok']);
     }
